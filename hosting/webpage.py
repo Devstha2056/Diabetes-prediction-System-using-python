b16c60ac8fdde07259595d5fde853007b436d031
Devstha2056 <104583360+Devstha2056@users.noreply.github.com>
2024-02-21 18:15:12 +0545

    Update webpage.py

--- hosting/webpage.py
+++ hosting/webpage.py
@@ -6,7 +6,7 @@ import streamlit as st
 
 
 # loading the saved model
-loaded_model = pickle.load(open("C:/Users/Devendra/.spyder-py3/hosting/trained_model.sav", 'rb'))
+loaded_model = pickle.load(open("hosting/trained_model.sav", 'rb'))
 
 
 # creating a function for Prediction
