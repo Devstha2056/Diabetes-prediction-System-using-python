f54f2834be19cda419bc3158a7ba8284b29a7e79
Devstha2056 <104583360+Devstha2056@users.noreply.github.com>
2024-02-29 12:36:40 +0545

    Update webpage.py

--- hosting/webpage.py
+++ hosting/webpage.py
@@ -2,61 +2,46 @@ import numpy as np
 import pickle
 import streamlit as st
 
-
-# loading the saved model
+# Loading the saved model
 loaded_model = pickle.load(open("hosting/trained_model.sav", 'rb'))
-
-
-# creating a function for Prediction
-
+# Creating a function for prediction
 def diabetes_prediction(input_data):
-    
 
-    # changing the input_data to numpy array
-    input_data_as_numpy_array = np.asarray(input_data)
-
-    # reshape the array as we are predicting for one instance
-    input_data_reshaped = input_data_as_numpy_array.reshape(1,-1)
+    # Reshape the array as we are predicting for one instance
+    input_data_reshaped = np.array(input_data).reshape(1, -1)
 
     prediction = loaded_model.predict(input_data_reshaped)
-    print(prediction)
 
-    if (prediction[0] == 0):
-      return 'The person is not diabetic'
+    if prediction[0] == 0:
+        return 'The person is not diabetic'
     else:
-      return 'The person is diabetic'
-  
-    
-  
+        return 'The person is diabetic'
+
 def main():
-    
-    
-    # giving a title
+    # Title of the web app
     st.title('Diabetes Prediction Web App')
     
+    # Input fields for user data with default value None
+    pregnancies = st.number_input('Number of Pregnancies', value=None, max_value=20)
+    glucose = st.number_input('Glucose Level', value=None, max_value=200)
+    blood_pressure = st.number_input('Blood Pressure value', value=None, max_value=150)
+    skin_thickness = st.number_input('Skin Thickness value', value=None, max_value=100)
+    insulin = st.number_input('Insulin Level', value=None, max_value=1000)
+    bmi = st.number_input('BMI value', value=None, max_value=60.0)
+    diabetes_pedigree_function = st.number_input('Diabetes Pedigree Function value', value=None, max_value=2.5)
+    age = st.number_input('Age of the Person', value=None, max_value=150)
     
-    # getting the input data from the user
-    
-    
-    Pregnancies = st.text_input('Number of Pregnancies')
-    Glucose = st.text_input('Glucose Level')
-    BloodPressure = st.text_input('Blood Pressure value')
-    SkinThickness = st.text_input('Skin Thickness value')
-    Insulin = st.text_input('Insulin Level')
-    BMI = st.text_input('BMI value')
-    DiabetesPedigreeFunction = st.text_input('Diabetes Pedigree Function value')
-    Age = st.text_input('Age of the Person')
-    
-    
-    # code for Prediction
     diagnosis = ''
-    
-    # creating a button for Prediction
-    
-    if st.button('Diabetes Test Result'):
-        diagnosis = diabetes_prediction([Pregnancies, Glucose, BloodPressure, SkinThickness, Insulin, BMI, DiabetesPedigreeFunction, Age])
-        
-        
+
+    # Creating a button for Prediction
+    if st.button('Test Result'):
+        # Check if any input field is empty
+        if None in [pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]:
+            st.error('Please fill in all input fields.')
+        else:
+            diagnosis = diabetes_prediction([pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age])
+
     st.success(diagnosis)
+
 if __name__ == '__main__':
     main()
