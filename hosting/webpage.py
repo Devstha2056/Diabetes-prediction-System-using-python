ec07ce48ca67a1b12508ce3a820cc4ff1c11eb6a
Devstha2056 <104583360+Devstha2056@users.noreply.github.com>
2024-02-29 17:06:49 +0545

    Update webpage.py

--- hosting/webpage.py
+++ hosting/webpage.py
@@ -17,17 +17,17 @@ def diabetes_prediction(input_data):
     prediction = loaded_model.predict(input_data_reshaped)
     # Interpret the prediction
     if prediction[0] == 0:
-        result = "Not Diabetic"
+        result = "**Not Diabetic**"
         doctor_suggestions = [
             "1. Based on the provided information, you seem to be at low risk of diabetes. However, it's always important to maintain a healthy lifestyle and have regular check-ups with your doctor.",
             "2. You have a balanced lifestyle. Keep up the good work and continue with healthy habits.",
             "3. Stay hydrated and make sure to get enough sleep for overall well-being.",
             "4. Consider regular health check-ups to monitor your health status.",
             "5. Regular exercise and a balanced diet are key to maintaining good health. Keep it up!",
-            "------------------Thank you----------------"
+            "**------------------Thank you----------------**"
         ]
     else:
-        result = "Diabetic"
+        result = "**Diabetic**"
         # Randomly select a doctor from the list
         doctor = np.random.choice(doctors)
         doctor_name = doctor["name"]
@@ -39,39 +39,39 @@ def diabetes_prediction(input_data):
             "2. Be more physically active.",
             "3. Eat healthy plant foods.",
             "4. Avoid sugary foods.",
-            "------------------Thank you----------------"
+            "**------------------Thank you----------------**"
         ]
     return result, doctor_suggestions
 
 def main():
   
-    st.title('Hospital Report: Diabetes Prediction')
+    st.title('**Hospital Report: Diabetes Prediction**')
 
-    st.header('Patient Information')
+    st.header('**Patient Information**')
     name = st.text_input('Name')
     gender = st.selectbox('Gender', ['Male', 'Female', 'Other'])
     address = st.text_area('Address')
     contact = st.text_area('Contact')
 
     st.write('---')  # Separator line
-    st.header('Medical Data')
+    st.header('**Medical Data**')
     pregnancies = st.number_input('Number of Pregnancies',  value=None,max_value=20)
     glucose = st.number_input('Glucose Level (mg/dL)', value=None, max_value=200)
     blood_pressure = st.number_input('Blood Pressure (mm Hg)', value=None, max_value=150)
-    skin_thickness = st.number_input('Skin Thickness (mm)', value=None, max_value=100)
+    skin_thickness = st.number_input('Skin Thickness (mm)', value=None, max_value=150)
     insulin = st.number_input('Insulin Level (mu U/ml)', value=None, max_value=1000)
-    bmi = st.number_input('BMI (Body Mass Index)', value=None, max_value=60.0)
+    bmi = st.number_input('BMI (Body Mass Index)', value=None, max_value=100.0)
     diabetes_pedigree_function = st.number_input('Diabetes Pedigree Function', value=None, max_value=2.5, format='%f')
     age = st.number_input('Age of the Person', value=None, max_value=150)
     st.write('---')  # Separator line
 
-    if st.button('Generate Report'):
+    if st.button('**Generate Report**'):
         if None in [pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age]:
             st.error('Please fill in all medical data fields.')
         else:
             result, doctor_suggestions = diabetes_prediction([pregnancies, glucose, blood_pressure, skin_thickness, insulin, bmi, diabetes_pedigree_function, age])
             # Display the report
-            st.header('Diabetes Prediction Report')
+            st.header('**Diabetes Prediction Report**')
             st.write(f'Patient Name: {name}')
             st.write(f'Gender: {gender}')
             st.write(f'Address: {address}')
@@ -88,7 +88,7 @@ def main():
             # st.write(f'pregnancies: {pregnancies}, glucose: {glucose}, blood_pressure: {blood_pressure}, skin_thickness: {skin_thickness}, insulin: {insulin}, bmi: {bmi}, diabetes_pedigree_function: {diabetes_pedigree_function}, age: {age}')
             st.write('---')
             st.write(f'Prediction Result: {result}')
-            st.write('Doctor Suggestions:')
+            st.write('**Doctor Suggestions:**')
             for suggestion in doctor_suggestions:
                 st.write(suggestion)
 
